fd8279e378435447f890d3be91667ba59a4b430a
Lithelyne <126444415+Lithelyne@users.noreply.github.com>
2023-03-21 17:28:33 -0700

    Version 1 likes button

---
+++ css_intro/Dojoweather/weather.js
@@ -0,0 +1,82 @@
+function displayAlert() {
+    alert("Loading weather report..")
+}
+
+function removeCookieSection() {
+    var cookie = document.querySelector(".cookie")
+    cookie.remove()
+}
+
+function switchTempMeasurements() {
+    var weathNum1 = document.querySelector(".weather-number-1")
+
+    if (element.value == "celcius") {
+        weatherNum1.innerText = ("24")
+    }
+    else {
+        weathNum1.innerText = ("75")
+    }
+
+    var weathNum2 = document.querySelector(".weather-number-2")
+
+    if (element.value == "celcius") {
+        weatherNum2.innerText = ("18")
+    }
+    else {
+        weathNum2.innerText = ("65")
+    }
+
+    var weathNum3 = document.querySelector(".weather-number-3")
+
+    if (element.value == "celcius") {
+        weatherNum3.innerText = ("27")
+    }
+    else {
+        weathNum3.innerText = ("80")
+    }
+
+    var weathNum1 = document.querySelector(".weather-number-4")
+
+
+    if (element.value == "celcius") {
+        weatherNum4.innerText = ("19")
+    }
+    else {
+        weathNum4.innerText = ("66")
+    }
+
+    var weathNum1 = document.querySelector(".weather-number-5")
+
+    if (element.value == "celcius") {
+        weatherNum5.innerText = ("21")
+    }
+    else {
+        weathNum5.innerText = ("69")
+    }
+
+    var weathNum1 = document.querySelector(".weather-number-6")
+
+    if (element.value == "celcius") {
+        weatherNum6.innerText = ("16")
+    }
+    else {
+        weathNum6.innerText = ("61")
+    }
+
+    var weathNum1 = document.querySelector(".weather-number-7")
+
+    if (element.value == "celcius") {
+        weatherNum7.innerText = ("26")
+    }
+    else {
+        weathNum7.innerText = ("78")
+    }
+
+    var weathNum1 = document.querySelector(".weather-number-8")
+
+    if (element.value == "celcius") {
+        weatherNum8.innerText = ("21")
+    }
+    else {
+        weathNum8.innerText = ("70")
+    }
---
+++ css_intro/Likes/likes.js
@@ -0,0 +1,29 @@
+var count1=9
+var countElement = document.querySelector("#count-1")
+
+console.log(countElement);
+
+function add1() {
+    count1++;
+    console.log(count1)
+}
+
+var count2=12
+var countElement = document.querySelector("#count-2")
+
+console.log(countElement);
+
+function add2() {
+    count2++;
+    console.log(count2)
+}
+
+var count3=9
+var countElement = document.querySelector("#count-3")
+
+console.log(countElement);
+
+function add3() {
+    count3++;
+    console.log(count3)
+}
