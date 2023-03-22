9b7b79a690a5b867257702bdfdec9b384952aa6b
Lithelyne <126444415+Lithelyne@users.noreply.github.com>
2023-03-21 19:17:02 -0700

    Redone Javascript Code

--- css_intro/Dojoweather/weather.js
+++ css_intro/Dojoweather/weather.js
@@ -1,82 +1,32 @@
-function displayAlert() {
-    alert("Loading weather report..")
+console.log("page loading...");
+
+var cookieDiv = document.querySelector(".cookie-policy");
+
+function loading() {
+    alert("Loading weather report...")
 }
 
-function removeCookieSection() {
-    var cookie = document.querySelector(".cookie")
-    cookie.remove()
+function accept() {
+    cookieDiv.remove();
 }
 
-function switchTempMeasurements() {
-    var weathNum1 = document.querySelector(".weather-number-1")
+function c2f(temp) {
+    return Math.round(9 / 5 * temp + 32);
+}
 
-    if (element.value == "celcius") {
-        weatherNum1.innerText = ("24")
-    }
-    else {
-        weathNum1.innerText = ("75")
-    }
+function f2c(temp) {
+    return Math.round(5 / 9 * (temp - 32));
+}
 
-    var weathNum2 = document.querySelector(".weather-number-2")
-
-    if (element.value == "celcius") {
-        weatherNum2.innerText = ("18")
+function convert(element) {
+    console.log(element.value);
+    for(var i=1; i<9; i++) {
+        var tempSpan = document.querySelector("#temp" + i);
+        var tempVal = parseInt(tempSpan.innerText);
+        if(element.value == "°C") {
+            tempSpan.innerText = f2c(tempVal);
+        } else {
+            tempSpan.innerText = c2f(tempVal);
+        }
     }
-    else {
-        weathNum2.innerText = ("65")
-    }
-
-    var weathNum3 = document.querySelector(".weather-number-3")
-
-    if (element.value == "celcius") {
-        weatherNum3.innerText = ("27")
-    }
-    else {
-        weathNum3.innerText = ("80")
-    }
-
-    var weathNum1 = document.querySelector(".weather-number-4")
-
-
-    if (element.value == "celcius") {
-        weatherNum4.innerText = ("19")
-    }
-    else {
-        weathNum4.innerText = ("66")
-    }
-
-    var weathNum1 = document.querySelector(".weather-number-5")
-
-    if (element.value == "celcius") {
-        weatherNum5.innerText = ("21")
-    }
-    else {
-        weathNum5.innerText = ("69")
-    }
-
-    var weathNum1 = document.querySelector(".weather-number-6")
-
-    if (element.value == "celcius") {
-        weatherNum6.innerText = ("16")
-    }
-    else {
-        weathNum6.innerText = ("61")
-    }
-
-    var weathNum1 = document.querySelector(".weather-number-7")
-
-    if (element.value == "celcius") {
-        weatherNum7.innerText = ("26")
-    }
-    else {
-        weathNum7.innerText = ("78")
-    }
-
-    var weathNum1 = document.querySelector(".weather-number-8")
-
-    if (element.value == "celcius") {
-        weatherNum8.innerText = ("21")
-    }
-    else {
-        weathNum8.innerText = ("70")
-    }
+}
